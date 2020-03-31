367a89c993c43796c927e7e532b17b8f23c9b292
高震雷 <gaozhenlei@bosssoft.com.cn>
2020-03-31 17:51:49 +0800

    20200331 提交

---
+++ common/log.py
@@ -0,0 +1,15 @@
+import logging
+from datetime import datetime
+import threading
+
+class Log(object):
+    def __init__(self):
+
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
